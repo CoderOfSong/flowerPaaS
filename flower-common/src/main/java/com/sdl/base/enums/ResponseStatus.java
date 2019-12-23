27873dc8b3f33fe24da703503e2969ea4af463b1
songdeling <songdeling@afcat.com.cn>
2019-12-23 17:10:06 +0800

    【新建(基础服务)】:主体框架搭建，服务项目搭建

---
+++ flower-common/src/main/java/com/sdl/base/enums/ResponseStatus.java
@@ -0,0 +1,76 @@
+package com.sdl.base.enums;
+
+public enum ResponseStatus {
+
+    /**
+     * Success
+     * 成功
+     */
+    SUCCESS(200, "Success"),
+
+    /**
+     * Failure
+     * 失败
+     */
+    FAILURE(500, "Failure"),
+
+    /**
+     * Bad Request
+     * 请求错误
+     */
+    BAD_REQUEST(400, "Bad Request"),
+
+    /**
+     * Unauthorized
+     * 未认证
+     */
+    UNAUTHORIZED(401, "Unauthorized"),
+
+    /**
+     * Forbidden
+     * 无权限
+     */
+    FORBIDDEN(403, "Forbidden"),
+
+    /**
+     * Not Found
+     * 请求不存在
+     */
+    NOT_FOUND(404, "Not Found"),
+
+    /**
+     * Method Not Allowed
+     * 方法不允许
+     */
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+
+    /**
+     * Request Timeout
+     * 请求超时
+     */
+    REQUEST_TIMEOUT(408, "Request Timeout"),
+
+    /**
+     * Too Many Requests
+     * 请求太多
+     */
+    TOO_MANY_REQUESTS(429, "Too Many Requests");
+
+    private final int code;
+
+    private final String msg;
+
+    ResponseStatus(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int code() {
+        return this.code;
+    }
+
+    public String msg() {
+        return this.msg;
+    }
+
+}
---
+++ flower-common/src/main/java/com/sdl/utils/utils/JsonUtil.java
@@ -0,0 +1,80 @@
+package com.sdl.utils.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @program flowerPaaS
+ * @description: Json处理工具类
+ * @author: songdeling
+ * @create: 2019/12/23 14:55
+ */
+@Component
+@Slf4j
+public class JsonUtil {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String jsonObj2Str(Object jsonObj) {
+        if (null == jsonObj) {
+            return null;
+        }
+        try {
+            final String str = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObj);
+            return str;
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static <T> T jsonStr2Obj(String jsonStr, Class<T> objType) {
+        if (StringUtils.isEmpty(jsonStr) || null == objType) {
+            return null;
+        }
+        try {
+            final T obj = objectMapper.readValue(jsonStr, objType);
+            return obj;
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static String getJsonStr(String json, String key) {
+        if (StringUtils.isEmpty(json) || StringUtils.isEmpty(key)) {
+            return null;
+        }
+        try {
+            final JsonNode jsonNode = objectMapper.readTree(json);
+            return getJsonNodeValue(jsonNode, key);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    private static String getJsonNodeValue(JsonNode jsonNode, String key) {
+        if (null == jsonNode || StringUtils.isEmpty(key)) {
+            return null;
+        }
+        int index = key.indexOf('.');
+        if (index == -1) {
+            if (null != jsonNode && null != jsonNode.get(key)) {
+                return jsonNode.get(key).toString();
+            }
+            return null;
+        } else {
+            String s1 = key.substring(0, index);
+            String s2 = key.substring(index + 1);
+            return getJsonNodeValue(jsonNode.get(s1), s2);
+        }
+    }
+
+}
