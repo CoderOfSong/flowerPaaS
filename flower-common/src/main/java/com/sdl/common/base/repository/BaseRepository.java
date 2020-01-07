0207e403f536f186ac15296a0193c24a2699430e
songdeling <songdeling@afcat.com.cn>
2020-01-07 09:31:16 +0800

    改为maven管理

--- flower-common/src/main/java/com/sdl/common/base/repository/BaseRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sdl.common.base.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
-}
--- flower-provider-service/flower-competence-service/src/main/java/com/sdl/comp/service/service/IRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.sdl.comp.service.service;
-
-import com.sdl.comp.service.entity.SysRole;
-
-import java.util.List;
-
-public interface IRoleService {
-    List<SysRole> getRoleByUserId(Integer userId);
-}
